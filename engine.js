af343bb08e7d14fe4c2848c75470401880c91e52
Molnár István <33826380+molnaristvan95@users.noreply.github.com>
2021-01-12 00:14:32 +0100

    Create engine.js

---
+++ engine.js
@@ -0,0 +1,164 @@
+var houseImg = 'https://lh3.googleusercontent.com/fife/ABSRlIoGWWC6fJo84o2QaFBwDnncT90FSaM_reDFkXW5vwI9XtDRua20k0i_9TrmyY9QJsuiWUKjCYMqkj0g7wHE7oeN9SiPuV1ccEkAJGNXQG852leJ3TELFaC3ybxLtl66CprcETBl5ZX4Uz6AtZS47Jp8oxw8a4oV5vdNyn9HB3dcSnI7nU_9E32S0h5QV0M4RdUzJLIVD7HNg4yssL8pGZPtZDdflmLt0aBJNf-tQREmGuf6zlx08iDyLeU2-bX0MxIHVdxwYe0lXEW-BhPvijAxqWR5GXNcbyssEqVUYk1MWsp-7814aEofuTu018QF1h28x1tH2vWrV12u9tTlMtFB8nQsCmANSj8vrMK1YFZC7aSBu3qsnlvuE-39qmdFtGXQXq2SukYuJnq1Zo2VSe0E3mG3Pt-Ta9OWWDkx4wnoCAKUSqx7yas1wZar-proAAmkmcU7C1l_N7ZV10lQunAb-eRCYVbbFXbT4EY1tTdaUa8bImj4rVIbXj_03sBLCYG7ILEPNH5Ucpv1WA7dwxqpEfTKH2ofpD7BA5q5NWy5Dp16pW5J3pF-q_JOCiNEk2lAlINMGdqE2w4gGEAyro06nrvmmOg_-eMlAxe8hc7_cISmLXrznXFRUx_EsopzzwAhkybs4FVXO0wk9MHMejNGIclkTyQ2Urr09wuZn_J6cfZGOz9JQ4Eyu2M1EE9IJZGsxiDeDTimLjCnuFuqUeaHSbTHyFVBChw=w1366-h624-ft',
+    tree = 'https://lh3.googleusercontent.com/fife/ABSRlIomW5P52QWnRUxy-KELcZoemzB3N-e9cSTWs1hZNBxsVvsmi9fMwJh994zYHIosRMuO-5j2cZgUoLA3VqMoFbiojmQNS5pFp5SRaT3U0xehKBi2qjGm_xYwIVCYD1X4DS4hDSN0Ggp6rfpOdKOychOAJXH0X_I0YAaORicb-x8rQ1m3teFEUUBYvqqTHnbwk4_V0cNOU8ZFRTxGjzfLJoim-0G7TIHJrVpHLV0Yz1qhyI-BJvqRMiKCCUCMhuVplqxfc1aoO0uBnDGg-pjIuEcCjxCsW2Dg8Tc1zKg24AC-whsR5uA7H_VpOc4Lnl0znFh-VBRrhXBcSWBqIwMG1gTmJGD258tvXN-h6uoHjX9F94AGqoI40MrzDAnzVOOadbeNxiizCXKuM170Zgmqf2Pvf3F0bvtul1ZLnXLh3V38jqPVcmfMNiUbFNrb2aVZT8IGAd1x2-7GghYu2NkSmdsTUOt_-xEfOg-vazDDjq9Zx1LJT_xlEpG2VYUzijfh5G3t_FR5RjlyYXB72ss76TIalfHgCU3AOpgcW3zJbFJjdiKb_luJpenBf2mZFIaue1o3VwDBcl4OI40D3ST41rrTQX-yE9paD3AGOptlGNgiEptmk8nabAxWoFkB0I3rQYCTAMS6-CsaK4sAq7yxINxCq9goBKI5EO5U-6SFDV3TO6V8OQk7whILb2d9EFzDKH8iHYvUpjmCkHNcB14krW1BZWW3S40niCg=w1366-h624-ft',
+    tree1 = 'https://lh3.googleusercontent.com/fife/ABSRlIqnsAmRcCZPQFuVyRyudtI_IWHWXMV-2kJZXDonIiHlGmbzJz_ZUG2O337pr5J86O2sI0pusgviEHAPMEQyYfZEEEpyQeVTWKqUq8Yk-EPiUj80FeeDiWyal-eP4FW1tmH206nEJKK0VBLIgHNRIoSyx8nXRuk9nqjAIh4KI1V1d38bXSdZpgigih6P0Tx_5PRYsyXy_cZ8yAyzmr2eD_LYKnT_qhoNJgpFfgr7rRWbkfbqpun0e0guof3tK4a4Tyjaz5LCu9Im9QerrRP0PXk3Rj9POvsU9yb9MDuWp_DIbPIWEmUggiyWLq_tu7pU0Vx6b5YoGJTHp7i9pWQAYUOx2scoDvz0AKblKDegLEthbYla253h3IyZVcal4I4X8324faoHhL9koqhmtsZIzHhglkjOiCmxU5RYSSmlUfnJVj7KdYny6NlQIqcyy4nG_UPQMeulze081ey3JC7WOfRyWGji3JZ4-v1TM_9IkfBbGZNeEU6wvyBt8LYv7dCXV3-KSkq5LJ8JHO297WM7IKk1RPkVddW_MsZTUIuX0Wt4hj11UEPF1VQkktAZ-ePSWB2XxZZbV_HQMnMs6BeKP0DiTRPejHCgkDkg7akcgsD_aqM5fkQbEflgH8-8aJufkc0V0ymeK15jJggowEjCoWrg7bt0L_vj4Kil7baQKGCNN_Y0c1b9Yw6f0WAXD0A-en7r879L87_JX0QvoMa2xfsMoXfJjfV8Gjs=w1366-h624-ft';
+
+function conveyorBelt() {
+  for (var c = 0; c < 5; c++) {
+    var i = document.createElement('div'),
+        brg = (Math.floor(Math.random() * (120 - 80 + 1)) + 80),
+        cont = (Math.floor(Math.random() * (120 - 80 + 1)) + 80);
+    houses.appendChild(i);
+    $(i).attr('class', 'houseMove');
+    $(i).css({'animation-delay':(c*4) + 's', 'z-index':(14-c), 'filter':'brightness(' + brg + '%) contrast(' + cont + '%)'});
+  }
+}
+conveyorBelt();
+
+var distance = 0;
+
+function trees() {
+  for (var fa = 0; fa < 5; fa++) {
+    var choice = (Math.floor(Math.random() * (10 - 2 + 1)) + 2),
+        tr = document.createElement('img'),
+        del = fa / 2,
+        back,
+        bright = (Math.floor(Math.random() * (170 - 60 + 1)) + 60);
+    houses.appendChild(tr);
+    $(tr).attr('class', 'tree');
+    if (choice > 5) {
+      back = tree;
+    } else {
+      back = tree1;
+    }
+    $(tr).attr('alt', 'fa kép 1 v 2');
+    $(tr).attr('src', back);
+    $(tr).css({'animation-delay':distance+'s', 'filter':'brightness(' + bright + '%) saturate(8)'});
+    distance += 1.5;
+  }
+}
+trees();
+
+housesCopy.innerHTML = houses.innerHTML;
+
+function rocks() {
+  for (var rs = 0; rs < 128; rs++) {
+    var r = document.createElement('div'),
+        rLt = 50.5,
+        rLt2 = (Math.floor(Math.random() * (100 - 0 + 1)) + 0),
+        alpha = (Math.floor(Math.random() * (9 - 1)) + 1),
+        radius = (Math.floor(Math.random() * (50 - 2 + 1)) + 2),
+        w = Math.floor(Math.random() * 6),
+        h = Math.floor(Math.random() * 6);
+    canvass.appendChild(r);
+    $(r).attr('class', 'smallRocks');
+    $(r).css({'animation':'rock' + rs + ' 7s infinite ease-in-out', 'left': rLt + '%', 'animation-delay':(rs / 8) + 's', 'background':'#000' + alpha, 'border-radius':radius + '%', 'width':w + 'px', 'height':h + 'px'});
+    keyfCSS.textContent += '@keyframes rock' + rs + '{ 0%{top:-5%; left:' + rLt + '%; background:#0000;} 30%{background:#000' + alpha + '}  100%{top:120%; left:' + rLt2 + '%} }';
+  }
+  return;
+}
+rocks();
+
+var birdC = document.getElementsByClassName('bird');
+
+function copyBirds() {
+  var birdC = document.getElementsByClassName('bird');
+  birdC[1].innerHTML = birdC[0].innerHTML;
+  birdC[2].innerHTML = birdC[0].innerHTML;
+}
+copyBirds();
+
+var bCounter = 0;
+
+function birds() {
+  var b = birdC;
+    var y = (Math.floor(Math.random() * (45 - 20 + 1)) + 20),
+        x = (Math.floor(Math.random() * (10 - 2 + 1)) + 2);
+    if (y > 32) {
+      $(b[bCounter]).css({'transform':'rotateZ(15deg)'});
+    } else {
+      $(b[bCounter]).css({'transform':'rotateZ(-15deg)'});
+    }
+    $(b[bCounter]).css({'top':y+'%'});
+  if (x > 5) {
+    $(b[bCounter]).css({'left':120+'%'});
+  } else {
+    $(b[bCounter]).css({'left': -20+'%'});
+  }
+  if (bCounter === 2 || bCounter > 2) {
+    bCounter = 0;
+  } else {
+    bCounter++
+  }
+}
+window.setInterval(birds, 5000);
+
+function sunLights() {
+  if (sunLight.checked === true) {
+    sun.style.display = 'block';
+  } else {
+    sun.style.display = 'none';
+  }
+}
+sunLight.addEventListener('click', sunLights);
+
+car.addEventListener('click', function() {
+  if (car.checked === true) {
+    carInside.style.display = 'block';
+  } else {
+    carInside.style.display = 'none';
+  }
+});
+
+function driveMe() {
+  var k = event.key,
+      wh = event.which,
+      hx = window.getComputedStyle(document.getElementById('houses')).getPropertyValue('left'),
+      n = hx.substring(0, (hx.length-2)),
+      int = parseInt(n, 10).toPrecision(4),
+      hx2 = window.getComputedStyle(document.getElementById('housesCopy')).getPropertyValue('left'),
+      n2 = hx2.substring(0, (hx2.length-2)),
+      int2 = parseInt(n2, 10).toPrecision(4),
+      speed = 3;
+  if (k === 'ArrowRight') {
+    var l1 = int - speed,
+        l2 = int2 - speed;
+    houses.style.left = l1 + 'px';
+    housesCopy.style.left = l2 + 'px';
+  }
+  if (wh === 37 || k === 'ArrowLeft') {
+    var l3 = int-(-speed),
+        l4 = int2-(-speed);
+    document.getElementById('houses').style.left = l3 + 'px';
+    document.getElementById('housesCopy').style.left = l4 + 'px';
+  }
+  return;
+}
+window.addEventListener('keydown', driveMe);
+
+function abort() {
+  for (var x = 0; x < document.getElementsByTagName('img').length; x++) {
+    document.getElementsByTagName('img')[x].setAttribute('draggable', false);
+  }
+  return;
+}
+abort();
+
+function centering() {
+  $('body').css({'display':'flex', 'align-items':'center', 'justify-content':'center'});
+  return;
+}
+document.body.addEventListener('dblclick', centering);
+
+function trash() {
+  $('#hide').css({'display':'none'});
+}
+window.setTimeout(trash, 2750);
+
+
+
+
+
+
+
+
+
